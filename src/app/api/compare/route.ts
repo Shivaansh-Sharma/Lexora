635d039e60bfc4b96840e095e96e0846afd6e5e2
Shivaansh-Sharma <shivaanshsharma.business@gmail.com>
2026-05-21 09:07:20 +0530

    Update route.ts

--- src/app/api/compare/route.ts
+++ src/app/api/compare/route.ts
@@ -23,7 +23,7 @@ async function getEmbedding(
 
   const response =
     await fetch(
-      "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2",
+      "https://router.huggingface.co/hf-inference/models/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2",
       {
         method: "POST",
 
